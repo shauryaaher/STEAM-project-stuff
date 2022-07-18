f7001297e75793b77827d1bcdf7c5477e95e0d2d
Shaurya Aher <shauryaaher1200@gmail.com>
2022-07-18 20:06:01 +0530

    HIGH when the IR sensor detects

--- STEAM-project-stuff.ino
+++ STEAM-project-stuff.ino
@@ -1,22 +1,28 @@
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   pinMode(2, INPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(13, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  if(digitalRead(2) ==  HIGH) {
-    digitalWrite(7, HIGH);
-    delay(1000);
-    digitalWrite(7, LOW);
-    digitalWrite(8, HIGH);
-    delay(1000);
-    digitalWrite(8, LOW);
-    digitalWrite(13, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW); 
+  if (digitalRead(2) == HIGH)
+  {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+  }
+  else
+  {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
   }
 }
